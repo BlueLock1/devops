b1860cd9a7fd5181a323379bc32b2f84b10ae76e
deathstrider941 <BRUCE.JIMENEZPABLO@GMAIL.COM>
2023-06-03 13:19:02 -0700

    New script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#New script
+
+echo new script
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+sudo apt-get install mysql-server
+
+sudo apt-get install mongodb.org
+
+sudo apt-get install apache2
+
+echo new script done!
